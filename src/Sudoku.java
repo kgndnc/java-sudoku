e5c7c9da207b39bed2b3a526ba8f332e912da4e3
kgndnc <hkagandnc@gmail.com>
2022-06-26 15:26:21 +0300

    fixed sudoku board placement

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -27,43 +27,27 @@ public class Sudoku {
 		}
 	}
 	
-	private static boolean isFilledWithUniqVals(int [] grid) {		
-		// Check if all numbers are used only once (except 0)	
-		Set<Integer> usedNums = new HashSet<Integer>();
+	@SuppressWarnings("unused")
+	private static boolean isValidNumberRow(int[][] board, int table, int row, int column, int number) {
+		/*		tables
+		 *   [0]  [1]  [2]  -> if one of these check [0,1,2][row]
+		 *   [3]  [4]  [5]  -> if one of these check [3,4,5][row]
+		 *   [6]  [7]  [8]  -> if one of these check [6,7,8][row]
+		 *      
+		 * */
 		
-		for (int i : grid) {			 
-			System.out.println(i);
-			if(i != 0 ) {
-				usedNums.add(i);
-			}
-		}		
-		System.out.println("Number of unique numbers: "  + usedNums.size());
-		boolean isFilled = usedNums.size() == 9 ? true : false;
 		
-		return isFilled;
 	}
 	
-	@SuppressWarnings("unused")
-	private static int[][] solveForOneBoard (int [][] table) {
+	private static boolean isFilledWithUniqVals(int[] grid) {		
 		// Check if all numbers are used only once (except 0)	
 		
-		// one set for current usable numbers in row
-		// one set for current usable numbers in column
-		// one set for current usable numbers in table
+	}
+	
+	
+	private static int[][] solveForOneBoard (int [][] table) {
+		// backtracking algorithm
 		
-		// Solve for one board then build your way up
-		
-		int [] currentBoard = table[0];
-		
-		for (int i = 0; i < currentBoard.length; i++) {
-			System.out.print(currentBoard[i] + "  ");
-			if ((i+1) % 3 == 0)
-				System.out.println("\n");
-		}
-		
-		for (int i : currentBoard) {			 
-			
-		}
 		
 		return table;
 	}
@@ -75,20 +59,22 @@ public class Sudoku {
 		// TODO Auto-generated method stub
 		
 		int [][] board = {
-			{0,0,5,0,0,0,0,0,8},
-			{8,0,0,4,0,0,3,5,2},
-			{9,0,4,0,0,5,0,0,0},
-			{0,0,9,6,0,0,4,0,0},
-			{0,0,0,0,0,0,0,0,0},
-			{0,0,6,0,0,3,7,0,0},
-			{0,0,0,8,0,0,3,0,6},
-			{6,3,9,0,0,1,0,0,5},
-			{2,0,0,0,0,0,4,0,0}
+			{0,0,5,8,0,0,9,0,4},
+			{0,0,0,4,0,0,0,0,5},
+			{0,0,8,3,5,2,0,0,0},			
+			{0,0,9,0,0,0,0,0,6},			
+			{6,0,0,0,0,0,0,0,3},
+			{4,0,0,0,0,0,7,0,0},			
+			{0,0,0,6,3,9,2,0,0},			
+			{8,0,0,0,0,1,0,0,0},
+			{3,0,6,0,0,5,4,0,0}
 		};
 		
-		solveForOneBoard(board);
+		// solveForOneBoard(board);
 		
-		 //showBoard(board);
+		 // showBoard(board);
+		
+		isValidNumberRow(board, 2, 6, 1, 1);
 		
 	}
 
